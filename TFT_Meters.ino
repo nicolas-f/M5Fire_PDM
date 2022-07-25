9cf4eee9a8f476c13e3b89621ab2b64e2b7a2236
nicolas-f <github_account@nettrader.fr>
2022-07-25 16:55:22 +0200

    less magic numbers

--- TFT_Meters.ino
+++ TFT_Meters.ino
@@ -12,6 +12,19 @@
 */
 
 #include <M5Stack.h>
+#include "driver/i2s.h"
+
+
+
+#define PIN_CLK  22
+#define PIN_DATA 21
+const i2s_port_t I2S_PORT = I2S_NUM_0;
+const int BLOCK_SIZE = 512;
+int16_t samples[BLOCK_SIZE];
+#define SAMPLE_RATE 24000
+long total_read = 0;
+
+
 
 #define TFT_GREY 0x5AEB
 
@@ -30,6 +43,75 @@ int old_analog  = -999;  // Value last displayed
 
 int d            = 0;
 
+int display_rms = 0;
+double display_dbspl = 0.0f;
+
+
+// http://www.schwietering.com/jayduino/filtuino/index.php?characteristic=be&passmode=hp&order=2&usesr=usesr&sr=24000&frequencyLow=100&noteLow=&noteHigh=&pw=pw&calctype=float&run=Send
+//High pass bessel filter order=2 alpha1=0.0041666666666667 
+class  FilterBeHp2
+{
+  public:
+    FilterBeHp2()
+    {
+      v[0]=0.0;
+      v[1]=0.0;
+    }
+  private:
+    float v[3];
+  public:
+    float step(float x) //class II 
+    {
+      v[0] = v[1];
+      v[1] = v[2];
+      v[2] = (9.823849154958753660e-1 * x)
+         + (-0.96497792085018785357 * v[0])
+         + (1.96456174113331383246 * v[1]);
+      return 
+         (v[0] + v[2])
+        - 2 * v[1];
+    }
+};
+
+FilterBeHp2 filter;
+
+void init_pdm() {
+  
+    esp_err_t err;
+
+    i2s_config_t audio_in_i2s_config = {
+         .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_PDM),
+         .sample_rate = SAMPLE_RATE,
+         .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+         .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT, // although the SEL config should be left, it seems to transmit on right
+         .communication_format = I2S_COMM_FORMAT_STAND_I2S,
+         .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1, // high interrupt priority
+         .dma_buf_count = 2,
+         .dma_buf_len = BLOCK_SIZE
+        };
+    
+    // This function must be called before any I2S driver read/write operations.
+    err = i2s_driver_install(I2S_PORT, &audio_in_i2s_config, 0, NULL);
+    if (err != ESP_OK) {
+      Serial.printf("Failed installing driver: %d\n", err);
+      while (true);
+    }
+  
+    // configure which pin on the ESP32 is connected to which pin on the mic:
+    i2s_pin_config_t audio_in_pin_config = {
+        .bck_io_num = I2S_PIN_NO_CHANGE, // not used
+        .ws_io_num = PIN_CLK,  // IO 15 clock pin
+        .data_out_num = I2S_PIN_NO_CHANGE, // Not used
+        .data_in_num = PIN_DATA   // data pin
+    };
+    
+    err = i2s_set_pin(I2S_PORT, &audio_in_pin_config);
+    if (err != ESP_OK) {
+      Serial.printf("Failed setting pin: %d\n", err);
+      while (true);
+    }
+}
+
 void print_number(double val, int precision, int x, int y) {
     char buf[16];
     dtostrf(val, 4, precision, buf);
@@ -38,17 +120,41 @@ void print_number(double val, int precision, int x, int y) {
     M5.Lcd.drawString(buf, x, y);  
 }
 
+void process_samples(void *pvParameters) {  
+    while(1){
+      size_t num_bytes_read;
+      i2s_read(I2S_PORT, 
+          (char *)samples, 
+          BLOCK_SIZE * 2,
+          &num_bytes_read,
+          portMAX_DELAY); // no timeout
+      if (num_bytes_read > 0) {            
+        int samples_read = num_bytes_read / 2;
+        total_read += samples_read;
+        float sample;
+        double rms = 0;
+        for(int i=0; i < samples_read; i++) {
+          sample = filter.step((float)(samples[i] / 32767));
+          rms += samples[i];
+        }            
+        display_rms = rms;
+        display_dbspl = 20 * log10(sqrt(rms / samples_read));
+      }
+    }
+}
+
 void setup(void) {
     M5.begin();
     M5.Power.begin();
     
+    // Initialize the I2S peripheral
+    init_pdm();
+    // Create a task that will read the data
+    xTaskCreatePinnedToCore(process_samples, "PDM_reader", 2048, NULL, 1, NULL, 1);
+
     M5.Lcd.fillScreen(TFT_BLACK);
 
     M5.Lcd.getSetup(tft_settings);
-
-    //print_number(tft_settings.tft_width, 0, 0, 150);
-    //print_number(tft_settings.tft_height, 0, 60, 150);
-    
 
     analogMeter(tft_settings.tft_height, tft_settings.tft_width, 0, 0);  // Draw analogue meter
     updateTime = millis();  // Next update time
@@ -191,8 +297,8 @@ void plotNeedle(int width, int height, int value, byte ms_delay) {
 
 
     M5.Lcd.setTextColor(TFT_BLACK, TFT_WHITE);
-    char buf[8];
-    dtostrf(value, 4, 0, buf);
+    char buf[40];
+    int l = sprintf(buf, "%d RMS", display_rms);
     M5.Lcd.setTextFont(2);
     M5.Lcd.setTextDatum(BL_DATUM);
     M5.Lcd.drawString(buf, border * 2, height * 0.6);
